93af656222342c24d99df1d6ccc40f9d46e0ff46
artem.polyvyanyy <artem.polyvyanyy@9b3f2bfc-f393-1fc5-1dc1-76278038343d>
2010-11-26 13:12:56 +0000

    Added cyclic example

---
+++ src/ee/ut/bpstruct/bpmn/test/cyclic/Model11427Test.java
@@ -0,0 +1,15 @@
+package ee.ut.bpstruct.bpmn.test.cyclic;
+
+import ee.ut.bpstruct.bpmn.test.StructuringTest;
+
+public class Model11427Test extends StructuringTest {
+
+	public Model11427Test() {
+		this.MODEL_NAME = "model11427";
+		this.MODEL_PATH_TPL = "models/cyclic/%s.bpmn";
+		this.OUTPUT_PATH_TPL = "tmp/cyclic/%s.dot";
+		this.CAN_STRUCTURE = true;
+	}
+
+}
+
