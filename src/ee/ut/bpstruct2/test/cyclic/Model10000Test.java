72abab286d0010d6acd7b6f4e8c0691f6a33d871
lgbanuelos <lgbanuelos@9b3f2bfc-f393-1fc5-1dc1-76278038343d>
2011-10-17 12:36:10 +0000

    Fixed JUnit test case: Model10000Test

--- src/ee/ut/bpstruct2/test/cyclic/Model10000Test.java
+++ src/ee/ut/bpstruct2/test/cyclic/Model10000Test.java
@@ -6,7 +6,7 @@ public class Model10000Test extends StructuringTest {
 
 	public Model10000Test() {
 		this.MODEL_NAME = "model10000";
-		this.MODEL_PATH_TPL = "models/cyclic/%s.json";
+		this.MODEL_PATH_TPL = "models/cyclic/%s.bpmn";
 		this.OUTPUT_PATH_TPL = "tmp/cyclic/%s.dot";
 		this.CAN_STRUCTURE = true;
 	}
